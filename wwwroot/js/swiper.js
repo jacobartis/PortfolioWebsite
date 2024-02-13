72e1061cad5c8e3ae845a2bbb4b5cb54dafa22d7
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-02-13 23:06:46 +0000

    Added swiper display for projects.

---
+++ wwwroot/js/swiper.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".swiper",{
+    effect: "flip",
+    grabCursor: true,
+    speed: 1000,
+    loop: true,
+    rotate: true,
+    autoplay: {
+        delay: 4000,
+        pauseOnMouseEnter: true,
+    },
+    spaceBetween:10,
+})
+
+swiper
