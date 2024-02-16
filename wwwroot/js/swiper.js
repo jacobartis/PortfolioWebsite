c7b312d49c1b1951434bf7b00df56485919caac3
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-02-16 13:41:10 +0000

    Added swiper for the skills section.

--- wwwroot/js/swiper.js
+++ wwwroot/js/swiper.js
@@ -1,14 +1,26 @@
-var swiper = new Swiper(".swiper",{
-    effect: "flip",
+var skillSwiper = new Swiper(".skill-swiper",{
+    grabCursor: true,
+    initialSlide: 3,
+    speed: 3000,
+    loop: true,
+    rotate: true,
+    slidesPerView: 5,
+    autoplay: {
+        delay: 10,
+        pauseOnMouseEnter: true,
+    },
+    spaceBetween:1,
+});
+
+var projSwiper = new Swiper(".proj-swiper",{
     grabCursor: true,
     speed: 1000,
     loop: true,
     rotate: true,
+    slidesPerView:3,
     autoplay: {
         delay: 4000,
         pauseOnMouseEnter: true,
     },
     spaceBetween:10,
-})
-
-swiper
+});
