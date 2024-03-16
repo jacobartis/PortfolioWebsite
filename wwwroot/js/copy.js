a1f3ce37e27f13e18a9fb40aa069efe7b1fc1b02
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-03-16 17:50:29 +0000

    Changed alert message for copying text.

--- wwwroot/js/copy.js
+++ wwwroot/js/copy.js
@@ -1,6 +1,6 @@
 function copyText(text){
     console.log(text);
     navigator.clipboard.writeText(text).then(function(x) {
-        alert("Link copied to clipboard: " + text);
+        alert("Copied text: " + text);
       });
 }
